f763d3fd592f427f6663e1b9a782b719eb47a1d4
Yukai Huang <yukaihuangtw@gmail.com>
2016-09-13 16:29:01 +0800

    setup bootstrap & jquery package

--- brunch-config.js
+++ brunch-config.js
@@ -6,10 +6,29 @@ module.exports = {
 				'app.js': /^app/
 			}
 		},
-		stylesheets: {joinTo: 'app.css'}
+		stylesheets: {
+			defaultExtensions: ['sass'],
+			joinTo: 'app.css'
+		}
 	},
 
 	plugins: {
-		babel: {presets: ['es2015']}
+		babel: {presets: ['es2015']},
+		sass: {
+			options: {
+				includePaths: ['node_modules/bootstrap/scss'], // tell sass-brunch where to look for files to @import
+				precision: 8 // minimum precision required by bootstrap-sass
+			}
+		},
+	},
+
+	npm: {
+		enabled: true,
+		whitelist: ['jquery', 'bootstrap-sass', 'tether'],
+		globals: { // bootstrap-sass' JavaScript requires both '$' and 'jQuery' in global scope
+			$: 'jquery',
+			jQuery: 'jquery',
+			Tether: 'tether'
+		}
 	}
 };
