a3ca6fa1a33ffb46669a50fafdee2680d8c77671
Yukai Huang <yukaihuangtw@gmail.com>
2016-09-13 15:47:51 +0800

    update project config

--- brunch-config.js
+++ brunch-config.js
@@ -1,15 +1,15 @@
 module.exports = {
-  files: {
-    javascripts: {
-      joinTo: {
-        'vendor.js': /^(?!app)/,
-        'app.js': /^app/
-      }
-    },
-    stylesheets: {joinTo: 'app.css'}
-  },
+	files: {
+		javascripts: {
+			joinTo: {
+				'vendor.js': /^(?!app)/,
+				'app.js': /^app/
+			}
+		},
+		stylesheets: {joinTo: 'app.css'}
+	},
 
-  plugins: {
-    babel: {presets: ['es2015']}
-  }
+	plugins: {
+		babel: {presets: ['es2015']}
+	}
 };
