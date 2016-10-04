69961992445d380f701fe68dd593a35341c25544
Yukai Huang <yukaihuangtw@gmail.com>
2016-10-04 11:22:17 +0800

    Update commit message

--- .travis/crawl_job.sh
+++ .travis/crawl_job.sh
@@ -49,7 +49,7 @@ fi
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
 git add --all
-git commit -m "Deploy to GitHub Pages: ${SHA}"
+git commit -m "Data Updats ${SHA}"
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
